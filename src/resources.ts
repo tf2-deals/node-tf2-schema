b5e6da3c552c7a8dd3dd01bf30ed354c69e4c6b7
Emir Güvenni <contact@emirguvenni.com>
2023-09-14 04:09:01 +0300

    Added resources

---
+++ src/resources.ts
@@ -0,0 +1,260 @@
+export const munitionCrate = new Map();
+munitionCrate
+  .set(82, 5734)
+  .set(83, 5735)
+  .set(84, 5742)
+  .set(85, 5752)
+  .set(90, 5781)
+  .set(91, 5802)
+  .set(92, 5803)
+  .set(103, 5859);
+
+export const pistolSkins = new Map();
+pistolSkins
+  .set(0, 15013)
+  .set(18, 15018)
+  .set(35, 15035)
+  .set(41, 15041)
+  .set(46, 15046)
+  .set(56, 15056)
+  .set(61, 15061)
+  .set(63, 15060)
+  .set(69, 15100)
+  .set(70, 15101)
+  .set(74, 15102)
+  .set(78, 15126)
+  .set(81, 15148);
+
+export const rocketLauncherSkins = new Map();
+rocketLauncherSkins
+  .set(1, 15014)
+  .set(6, 15006)
+  .set(28, 15028)
+  .set(43, 15043)
+  .set(52, 15052)
+  .set(57, 15057)
+  .set(60, 15081)
+  .set(69, 15104)
+  .set(70, 15105)
+  .set(76, 15129)
+  .set(79, 15130)
+  .set(80, 15150);
+
+export const medicgunSkins = new Map();
+medicgunSkins
+  .set(2, 15010)
+  .set(5, 15008)
+  .set(25, 15025)
+  .set(39, 15039)
+  .set(50, 15050)
+  .set(65, 15078)
+  .set(72, 15097)
+  .set(76, 15120)
+  .set(78, 15121)
+  .set(79, 15122)
+  .set(81, 15145)
+  .set(83, 15146);
+
+export const revolverSkins = new Map();
+revolverSkins
+  .set(3, 15011)
+  .set(27, 15027)
+  .set(42, 15042)
+  .set(51, 15051)
+  .set(63, 15064)
+  .set(64, 15062)
+  .set(65, 15063)
+  .set(72, 15103)
+  .set(76, 15127)
+  .set(77, 15128)
+  .set(81, 15149);
+
+export const stickybombSkins = new Map();
+stickybombSkins
+  .set(4, 15012)
+  .set(8, 15009)
+  .set(24, 15024)
+  .set(38, 15038)
+  .set(45, 15045)
+  .set(48, 15048)
+  .set(60, 15082)
+  .set(62, 15083)
+  .set(63, 15084)
+  .set(68, 15113)
+  .set(76, 15137)
+  .set(78, 15138)
+  .set(81, 15155);
+
+export const sniperRifleSkins = new Map();
+sniperRifleSkins
+  .set(7, 15007)
+  .set(14, 15000)
+  .set(19, 15019)
+  .set(23, 15023)
+  .set(33, 15033)
+  .set(59, 15059)
+  .set(62, 15070)
+  .set(64, 15071)
+  .set(65, 15072)
+  .set(76, 15135)
+  .set(66, 15111)
+  .set(67, 15112)
+  .set(78, 15136)
+  .set(82, 15154);
+
+export const flameThrowerSkins = new Map();
+flameThrowerSkins
+  .set(9, 15005)
+  .set(17, 15017)
+  .set(30, 15030)
+  .set(34, 15034)
+  .set(49, 15049)
+  .set(54, 15054)
+  .set(60, 15066)
+  .set(61, 15068)
+  .set(62, 15067)
+  .set(66, 15089)
+  .set(67, 15090)
+  .set(76, 15115)
+  .set(80, 15141);
+
+export const minigunSkins = new Map();
+minigunSkins
+  .set(10, 15004)
+  .set(20, 15020)
+  .set(26, 15026)
+  .set(31, 15031)
+  .set(40, 15040)
+  .set(55, 15055)
+  .set(61, 15088)
+  .set(62, 15087)
+  .set(63, 15086)
+  .set(70, 15098)
+  .set(73, 15099)
+  .set(76, 15123)
+  .set(77, 15125)
+  .set(78, 15124)
+  .set(84, 15147);
+
+export const scattergunSkins = new Map();
+scattergunSkins
+  .set(11, 15002)
+  .set(15, 15015)
+  .set(21, 15021)
+  .set(29, 15029)
+  .set(36, 15036)
+  .set(53, 15053)
+  .set(61, 15069)
+  .set(63, 15065)
+  .set(69, 15106)
+  .set(72, 15107)
+  .set(74, 15108)
+  .set(76, 15131)
+  .set(83, 15157)
+  .set(85, 15151);
+
+export const shotgunSkins = new Map();
+shotgunSkins
+  .set(12, 15003)
+  .set(16, 15016)
+  .set(44, 15044)
+  .set(47, 15047)
+  .set(60, 15085)
+  .set(72, 15109)
+  .set(76, 15132)
+  .set(78, 15133)
+  .set(86, 15152);
+
+export const smgSkins = new Map();
+smgSkins
+  .set(13, 15001)
+  .set(22, 15022)
+  .set(32, 15032)
+  .set(37, 15037)
+  .set(58, 15058)
+  .set(65, 15076)
+  .set(69, 15110)
+  .set(79, 15134)
+  .set(81, 15153);
+
+export const wrenchSkins = new Map();
+wrenchSkins
+  .set(60, 15074)
+  .set(61, 15073)
+  .set(64, 15075)
+  .set(75, 15114)
+  .set(77, 15140)
+  .set(78, 15139)
+  .set(82, 15156);
+
+export const grenadeLauncherSkins = new Map();
+grenadeLauncherSkins
+  .set(60, 15077)
+  .set(63, 15079)
+  .set(67, 15091)
+  .set(68, 15092)
+  .set(76, 15116)
+  .set(77, 15117)
+  .set(80, 15142)
+  .set(84, 15158);
+
+export const knifeSkins = new Map();
+knifeSkins
+  .set(64, 15080)
+  .set(69, 15094)
+  .set(70, 15095)
+  .set(71, 15096)
+  .set(77, 15119)
+  .set(78, 15118)
+  .set(81, 15143)
+  .set(82, 15144);
+
+export const exclusiveGenuine = new Map();
+exclusiveGenuine
+  .set(810, 831) // Genuine Red-Tape Recorder
+  .set(811, 832) // Genuine Huo-Long Heater
+  .set(812, 833) // Genuine Flying Guillotine
+  .set(813, 834) // Genuine Neon Annihilator
+  .set(814, 835) // Genuine Triad Trinket
+  .set(815, 836) // Genuine Champ Stamp
+  .set(816, 837) // Genuine Marxman
+  .set(817, 838) // Genuine Human Cannonball
+  .set(30720, 30740) // Genuine Arkham Cowl
+  .set(30721, 30741) // Genuine Firefly
+  .set(30724, 30739); // Genuine Fear Monger
+
+export const exclusiveGenuineReversed = new Map();
+exclusiveGenuineReversed
+  .set(831, 810) // Red-Tape Recorder
+  .set(832, 811) // Huo-Long Heater
+  .set(833, 812) // Flying Guillotine
+  .set(834, 813) // Neon Annihilator
+  .set(835, 814) // Triad Trinket
+  .set(836, 815) // Champ Stamp
+  .set(837, 816) // Marxman
+  .set(838, 817) // Human Cannonball
+  .set(30740, 30720) // Arkham Cowl
+  .set(30741, 30721) // Firefly
+  .set(30739, 30724); // Fear Monger
+
+export const retiredKeys = {
+  5049: { defindex: 5049, name: 'Festive Winter Crate Key' },
+  5067: { defindex: 5067, name: 'Refreshing Summer Cooler Key' },
+  5072: { defindex: 5072, name: 'Naughty Winter Crate Key' },
+  5073: { defindex: 5073, name: 'Nice Winter Crate Key' },
+  5079: { defindex: 5079, name: 'Scorched Key' },
+  5081: { defindex: 5081, name: 'Fall Key' },
+  5628: { defindex: 5628, name: 'Eerie Key' },
+  5631: { defindex: 5631, name: 'Naughty Winter Crate Key 2012' },
+  5632: { defindex: 5632, name: 'Nice Winter Crate Key 2012' },
+  5713: { defindex: 5713, name: 'Spooky Key' }, // Non-Craftable
+  5716: { defindex: 5716, name: 'Naughty Winter Crate Key 2013' }, // Non-Craftable
+  5717: { defindex: 5717, name: 'Nice Winter Crate Key 2013' }, // Non-Craftable
+  5762: { defindex: 5762, name: 'Limited Late Summer Crate Key' }, // Non-Craftable
+  5791: { defindex: 5791, name: 'Naughty Winter Crate Key 2014' },
+  5792: { defindex: 5792, name: 'Nice Winter Crate Key 2014' },
+} as const;
+
+export const retiredKeysNames = Object.values(retiredKeys).map((retiredKey) =>
+  retiredKey.name.toLowerCase(),
+);
